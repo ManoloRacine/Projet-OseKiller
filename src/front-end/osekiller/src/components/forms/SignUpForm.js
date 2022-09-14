61a125c86a7acdb9edb057ad5516fc90a14b5ff4
ManoloRacine <manolo191202@gmail.com>
2022-09-14 16:13:12 -0400

    OS-36 added form validation to sign up

--- src/front-end/osekiller/src/components/forms/SignUpForm.js
+++ src/front-end/osekiller/src/components/forms/SignUpForm.js
@@ -1,44 +1,164 @@
 import { useState } from "react";
+import React from "react";
+import { useFormik } from 'formik';
+import * as Yup from "yup"
 
 const SignUpForm = (props) => {
     const [userType, setUserType] = useState("etudiant");
+    
+    const formikNormal = useFormik({
+        initialValues : {
+            nom : "",
+            prenom : "",
+            email : "",
+            password : "",
+            passwordConfirmation : ""
+        },
+        validationSchema : Yup.object({
+            nom : Yup.string().required("Requis"),
+            prenom : Yup.string().required("Requis"),
+            email : Yup.string().required("Requis").email("Pas un email valide"),
+            password : Yup.string().required("Requis"),
+            passwordConfirmation : Yup.string().required("Requis").oneOf([Yup.ref('password')], "le mot de passe n'est pas le même"),
+        }),
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+          },
+    });
+
+    const formikCompany = useFormik({
+        initialValues : {
+            nom : "",
+            email : "",
+            password : "",
+            passwordConfirmation : ""
+        },
+        validationSchema : Yup.object({
+            nom : Yup.string().required("Requis"),
+            email : Yup.string().required("Requis").email("Pas un email valide"),
+            password : Yup.string().required("Requis"),
+            passwordConfirmation : Yup.string().required("Requis").oneOf([Yup.ref('password')], "le mot de passe n'est pas le même"),
+        }),
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+          },
+    });
+
 
     function formType(value) {
         switch (value) {
             case "etudiant":
             case "gestionnaire":
-                return <div className="col sm-8">
+                return <form className="col sm-8" onSubmit={formikNormal.handleSubmit}>
                         <div className="input-group pb-2">
-                            <input className="form-control" placeholder="NOM"></input>
+                            <input name="nom"
+                            id="nom"
+                            type="text"
+                            onChange={formikNormal.handleChange}
+                            value={formikNormal.values.nom} 
+                            onBlur={formikNormal.handleBlur}
+                            placeholder="NOM">
+                            </input>
+                            {formikNormal.touched.nom && formikNormal.errors.nom ? <div>{formikNormal.errors.nom}</div> : null}
                         </div>
                         <div className="input-group pb-2">
-                            <input className="form-control" placeholder="PRÉNOM"></input>
+                        <input name="prenom"
+                            id="prenom"
+                            type="text"
+                            onChange={formikNormal.handleChange}
+                            value={formikNormal.values.prenom} 
+                            onBlur={formikNormal.handleBlur}
+                            placeholder="PRÉNOM">
+                            </input>
+                            {formikNormal.touched.prenom && formikNormal.errors.prenom ? <div>{formikNormal.errors.prenom}</div> : null}
                         </div>
                         <div className="input-group pb-2">
-                            <input className="form-control" placeholder="E-MAIL"></input>
+                        <input name="email"
+                            id="email"
+                            type="text"
+                            onChange={formikNormal.handleChange}
+                            value={formikNormal.values.email} 
+                            onBlur={formikNormal.handleBlur}
+                            placeholder="E-MAIL">
+                            </input>
+                            {formikNormal.touched.email && formikNormal.errors.email ? <div>{formikNormal.errors.email}</div> : null}
                         </div>
                         <div className="input-group pb-2">
-                            <input className="form-control" placeholder="MOT DE PASSE"></input>
+                        <input name="password"
+                            id="password"
+                            type="text"
+                            onChange={formikNormal.handleChange}
+                            value={formikNormal.values.password} 
+                            onBlur={formikNormal.handleBlur}
+                            placeholder="MOT DE PASSE">
+                            </input>
+                            {formikNormal.touched.password && formikNormal.errors.password ? <div>{formikNormal.errors.password}</div> : null}
                         </div>
                         <div className="input-group pb-2">
-                            <input className="form-control" placeholder="CONFIRMATION MOT DE PASSE"></input>
+                        <input name="passwordConfirmation"
+                            id="passwordConfirmation"
+                            type="text"
+                            onChange={formikNormal.handleChange}
+                            value={formikNormal.values.passwordConfirmation} 
+                            onBlur={formikNormal.handleBlur}
+                            placeholder="CONFIRMATION MOT DE PASSE">
+                            </input>
+                            {formikNormal.touched.passwordConfirmation && formikNormal.errors.passwordConfirmation ? <div>{formikNormal.errors.passwordConfirmation}</div> : null}
                         </div>
-                    </div>
+                        <div className="input-group">
+                            <button type="submit">Soumettre</button>
+                        </div>
+                    </form>
             case "compagnie":
-                return <div className="col sm-8">
+                return <form className="col sm-8" onSubmit={formikCompany.handleSubmit}>
                     <div className="input-group pb-2">
-                        <input className="form-control" placeholder="NOM DE LA COMPAGNIE"></input>
+                        <input name="nom"
+                        id="nom"
+                        type="text"
+                        onChange={formikCompany.handleChange}
+                        value={formikCompany.values.nom} 
+                        onBlur={formikCompany.handleBlur}
+                        placeholder="NOM DE LA COMPAGNIE">
+                        </input>
+                        {formikCompany.touched.nom && formikCompany.errors.nom ? <div>{formikCompany.errors.nom}</div> : null}
                     </div>
                     <div className="input-group pb-2">
-                        <input className="form-control" placeholder="E-MAIL"></input>
+                    <input name="email"
+                        id="email"
+                        type="text"
+                        onChange={formikCompany.handleChange}
+                        value={formikCompany.values.email} 
+                        onBlur={formikCompany.handleBlur}
+                        placeholder="E-MAIL">
+                        </input>
+                        {formikCompany.touched.email && formikCompany.errors.email ? <div>{formikCompany.errors.email}</div> : null}
                     </div>
                     <div className="input-group pb-2">
-                        <input className="form-control" placeholder="MOT DE PASSE"></input>
+                    <input name="password"
+                        id="password"
+                        type="text"
+                        onChange={formikCompany.handleChange}
+                        value={formikCompany.values.password} 
+                        onBlur={formikCompany.handleBlur}
+                        placeholder="MOT DE PASSE">
+                        </input>
+                        {formikCompany.touched.password && formikCompany.errors.password ? <div>{formikCompany.errors.password}</div> : null}
                     </div>
                     <div className="input-group pb-2">
-                        <input className="form-control" placeholder="CONFIRMATION MOT DE PASSE"></input>
+                    <input name="passwordConfirmation"
+                        id="passwordConfirmation"
+                        type="text"
+                        onChange={formikCompany.handleChange}
+                        value={formikCompany.values.passwordConfirmation} 
+                        onBlur={formikCompany.handleBlur}
+                        placeholder="CONFIRMATION MOT DE PASSE">
+                        </input>
+                        {formikCompany.touched.passwordConfirmation && formikCompany.errors.passwordConfirmation ? <div>{formikCompany.errors.passwordConfirmation}</div> : null}
                     </div>
-                </div>
+                    <div className="input-group">
+                        <button type="submit">Soumettre</button>
+                    </div>
+                </form>
             default:
                 return <h1>ERROR</h1>
         }
