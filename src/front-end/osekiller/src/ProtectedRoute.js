3642bb2e091865fb7e5113034120d3ff069842ed
Jérémy Mailhot <jaytech64@pm.me>
2022-09-28 11:48:18 -0400

    Complétion du design + bug fix token expiré

--- src/front-end/osekiller/src/ProtectedRoute.js
+++ src/front-end/osekiller/src/ProtectedRoute.js
@@ -1,6 +1,24 @@
 import { Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { pingToken } from "../src/services/AuthService";
 
 const ProtectedRoute = ({ children }) => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        pingToken()
+            .then(() => {
+                return children;
+            })
+            .catch((err) => {
+                if (err.response.status === 403) {
+                    console.log("Token expiré");
+                    navigate("/");
+                }
+            });
+    }, [children, navigate]);
+
     const token = localStorage.getItem("accessToken");
 
     if (!token) {
--- src/front-end/osekiller/src/views/UploadCv.js
+++ src/front-end/osekiller/src/views/UploadCv.js
@@ -1,7 +1,11 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { faArrowLeft, faCloudArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const UploadCv = () => {
     const [selectedFiles, setSelectedFiles] = useState([]);
+    const navigate = useNavigate();
 
     const showSelectedFiles = ({ target }) => {
         const uploadedFiles = Array.from(target.files);
@@ -18,7 +22,16 @@ const UploadCv = () => {
                 className="d-flex flex-column col-sm-10 p-4 rounded text-white"
                 style={{ backgroundColor: "#2C324C", minHeight: "90vh" }}
             >
-                <h1 className="text-center">Téléverser votre CV</h1>
+                <section className="mb-4">
+                    <button
+                        className="btn btn-primary float-left"
+                        onClick={() => navigate("/dashboard")}
+                    >
+                        <FontAwesomeIcon icon={faArrowLeft} className="me-2" />
+                        Dashboard
+                    </button>
+                    <h1 className="text-center">Téléverser votre CV</h1>
+                </section>
                 <form className="mb-3">
                     <label
                         htmlFor="formFileMultiple"
@@ -34,14 +47,30 @@ const UploadCv = () => {
                         multiple
                     />
                 </form>
-                <section id="selectedCv">
-                    <ul className="p-0">
-                        {selectedFiles.map((fileName, i) => (
-                            <li key={i}>{fileName}</li>
-                        ))}
-                    </ul>
+                <section
+                    id="selectedCv"
+                    className={selectedFiles.length === 0 ? "my-auto" : ""}
+                >
+                    {selectedFiles.length === 0 ? (
+                        <FontAwesomeIcon
+                            icon={faCloudArrowUp}
+                            className="fa-10x d-flex mx-auto"
+                        />
+                    ) : (
+                        <ul className="p-0">
+                            {selectedFiles.map((fileName, i) => (
+                                <li key={i}>{fileName}</li>
+                            ))}
+                        </ul>
+                    )}
                 </section>
-                <section className="d-flex flex-row-reverse align-items-bottom mt-auto">
+                <section
+                    className={
+                        selectedFiles.length === 0
+                            ? "d-flex flex-row-reverse align-items-bottom"
+                            : "d-flex flex-row-reverse align-items-bottom mt-auto"
+                    }
+                >
                     <button
                         className="btn"
                         style={{ backgroundColor: "#ee7600" }}
