561569bc65678142fa77b48ba8e3283f82554f50
ManoloRacine <manolo191202@gmail.com>
2022-09-28 15:17:28 -0400

    OS-43 ajustement d'un test UserController

--- src/test/java/com/osekiller/projet/controller/UserControllerTest.java
+++ src/test/java/com/osekiller/projet/controller/UserControllerTest.java
@@ -34,6 +34,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.server.ResponseStatusException;
+import static org.hamcrest.Matchers.is;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -53,17 +54,49 @@ public class UserControllerTest {
     void getUsersHappyDay() throws Exception {
         Student mockUser1 = new Student("test1", "test@student.com", "1") ;
         mockUser1.setRole(new Role("STUDENT"));
+        mockUser1.setId(1L);
         UserDto mockDto1 = new UserDto(mockUser1.getEmail(), mockUser1.getName(), mockUser1.isEnabled(), mockUser1.getId(), mockUser1.getRole().getName()) ;
         Manager mockUser2 = new Manager("test2", "test@manager.com", "1") ;
         mockUser2.setRole(new Role("MANAGER"));
+        mockUser2.setId(2L);
         UserDto mockDto2 = new UserDto(mockUser2.getEmail(), mockUser2.getName(), mockUser2.isEnabled(), mockUser2.getId(), mockUser2.getRole().getName()) ;
         Company mockUser3 = new Company("test3", "test@company.com", "1") ;
         mockUser3.setRole(new Role("COMPANY"));
+        mockUser3.setId(3L);
         UserDto mockDto3 = new UserDto(mockUser3.getEmail(), mockUser3.getName(), mockUser3.isEnabled(), mockUser3.getId(), mockUser3.getRole().getName()) ;
         List<UserDto> mockDtoList = new ArrayList<UserDto>(Arrays.asList(mockDto1, mockDto2, mockDto3)) ;
         doReturn(mockDtoList).when(userService).getUsers() ;
 
         mockMvc.perform(get("/users"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].email", is("test@student.com")))
+                .andExpect(jsonPath("$[0].name", is("test1")))
+                .andExpect(jsonPath("$[0].enabled", is(false)))
+                .andExpect(jsonPath("$[0].role", is("STUDENT")))
+                .andExpect(jsonPath("$[1].id", is(2)))
+                .andExpect(jsonPath("$[1].email", is("test@manager.com")))
+                .andExpect(jsonPath("$[1].name", is("test2")))
+                .andExpect(jsonPath("$[1].enabled", is(false)))
+                .andExpect(jsonPath("$[1].role", is("MANAGER")))
+                .andExpect(jsonPath("$[2].id", is(3)))
+                .andExpect(jsonPath("$[2].email", is("test@company.com")))
+                .andExpect(jsonPath("$[2].name", is("test3")))
+                .andExpect(jsonPath("$[2].enabled", is(false)))
+                .andExpect(jsonPath("$[2].role", is("COMPANY"))) ;
+    }
+
+    @Test
+    @WithMockUser
+    void getUsersEmpty() throws Exception {
+        List<UserDto> mockDtoList = new ArrayList<>() ;
+        doReturn(mockDtoList).when(userService).getUsers() ;
+
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", is(new ArrayList()))) ;
     }
 
     @Test
