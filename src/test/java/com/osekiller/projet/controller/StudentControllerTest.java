d4aa2d1d42fe34b264a977d8af29001e57d07949
ManoloRacine <manolo191202@gmail.com>
2022-09-28 23:38:26 -0400

    OS-46 ajout test controller save CV

--- src/test/java/com/osekiller/projet/controller/StudentControllerTest.java
+++ src/test/java/com/osekiller/projet/controller/StudentControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.io.Resource;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -16,6 +17,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -40,5 +42,14 @@ public class StudentControllerTest {
         mockMvc.perform(get("/student/{id}/cv", 1L))
                 .andExpect(status().isOk()) ;
 
+    }
+
+    @Test
+    @WithMockUser
+    void saveCVHappyDay() throws Exception {
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "test.txt", "text/plain", "test".getBytes()) ;
+        mockMvc.perform(put("/student/{id}/cv", 1L)
+                        .content(mockMultipartFile.getBytes()))
+                .andExpect(status().isOk()) ;
     }
 }
