ebf73cb2c59ec819a49a28cddaec0f5ca0fe27b7
Jérémy Mailhot <jaytech64@pm.me>
2022-10-11 11:57:06 -0400

    Ajout du formulaire

--- src/front-end/osekiller/src/services/UploadService.js
+++ src/front-end/osekiller/src/services/UploadService.js
@@ -9,11 +9,15 @@ export const uploadCv = async (data, studentId) => {
     });
 };
 
-export const uploadInternshipOffer = async (data, companyId) => {
-    return axios.post(`/compagnies/${companyId}/offers`, data, {
-        headers: {
-            "Content-Type": "multipart/form-data",
-            Authorization: localStorage.getItem("accessToken"),
-        },
-    });
+export const uploadInternshipOffer = async (dto, data, companyId) => {
+    return axios.post(
+        `/companies/${companyId}/offers`,
+        { offerDto: JSON.stringify(dto), file: data },
+        {
+            headers: {
+                "Content-Type": "multipart/form-data",
+                Authorization: localStorage.getItem("accessToken"),
+            },
+        }
+    );
 };
--- src/front-end/osekiller/src/views/UploadInternship.js
+++ src/front-end/osekiller/src/views/UploadInternship.js
@@ -1,31 +1,54 @@
 import Upload from "../components/Upload";
 import { useState } from "react";
 import { uploadInternshipOffer } from "../services/UploadService";
+import UploadInternshipForm from "../components/forms/UploadInternshipForm";
 
 const UploadInternship = () => {
+    const [position, setPosition] = useState("");
+    const [salary, setSalary] = useState("");
+    const [startDate, setStartDate] = useState("");
+    const [endDate, setEndDate] = useState("");
     const [selectedFile, setSelectedFile] = useState({});
     const [isOfferSubmitted, setIsOfferSubmitted] = useState(false);
 
     const handleSubmit = (userId) => {
+        const dto = {
+            position: position,
+            salary: parseFloat(salary),
+            startDate: startDate,
+            endDate: endDate,
+        };
         const formData = new FormData();
         formData.append("file", selectedFile);
-        uploadInternshipOffer(formData, userId)
+        uploadInternshipOffer(dto, formData, userId)
             .then(() => setIsOfferSubmitted(true))
 
             .catch((err) => console.log("Error:", err));
     };
     return (
-        <Upload
-            subtitle={"Téléverser une offre de stage"}
-            title={"Choisir l'offre de stage"}
-            selectedFile={selectedFile}
-            onChange={({ target }) =>
-                setSelectedFile(Array.from(target.files)[0])
-            }
-            onDelete={() => setSelectedFile({})}
-            onSubmit={handleSubmit}
-            isSubmitted={isOfferSubmitted}
-        />
+        <div>
+            <UploadInternshipForm
+                setStartDate={({ target }) => setStartDate(target.value)}
+                endDate={endDate}
+                position={position}
+                salary={salary}
+                setEndDate={({ target }) => setEndDate(target.value)}
+                setPosition={({ target }) => setPosition(target.value)}
+                startDate={startDate}
+                setSalary={({ target }) => setSalary(target.value)}
+            />
+            <Upload
+                subtitle={"Téléverser une offre de stage"}
+                title={"Choisir l'offre de stage"}
+                selectedFile={selectedFile}
+                onChange={({ target }) =>
+                    setSelectedFile(Array.from(target.files)[0])
+                }
+                onDelete={() => setSelectedFile({})}
+                onSubmit={handleSubmit}
+                isSubmitted={isOfferSubmitted}
+            />
+        </div>
     );
 };
 
