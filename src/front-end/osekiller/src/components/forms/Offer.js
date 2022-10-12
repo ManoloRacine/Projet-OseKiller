bbfa65e3bdf7858f7261f82413cb11d84a1ad366
Jérémy Mailhot <jaytech64@pm.me>
2022-10-12 11:37:19 -0400

    OS-60 Amélioration du UI de Offer.js

--- src/front-end/osekiller/src/components/forms/Offer.js
+++ src/front-end/osekiller/src/components/forms/Offer.js
@@ -3,18 +3,46 @@ import PropTypes from "prop-types";
 
 const Offer = ({ position, salary, startDate, endDate }) => {
     return (
-        <div>
-            <p>{position}</p>
-            <p>{salary}</p>
-            <p>{startDate}</p>
-            <p>{endDate}</p>
+        <div
+            className={
+                "offer d-flex justify-content-evenly text-white my-4 py-4 rounded"
+            }
+            style={{ backgroundColor: "#2C324C" }}
+        >
+            <div>
+                <p className={"fs-4 text-decoration-underline"}>Position</p>
+                <p>{position}</p>
+            </div>
+            <div>
+                <p className={"fs-4 text-decoration-underline"}>Salaire</p>
+                <p>{salary}</p>
+            </div>
+            <div>
+                <p className={"fs-4 text-decoration-underline"}>
+                    Date de début
+                </p>
+                <p>{startDate}</p>
+            </div>
+            <div>
+                <p className={"fs-4 text-decoration-underline"}>Date de fin</p>
+                <p>{endDate}</p>
+            </div>
+
+            <div>
+                <button className={"btn btn-success d-block mb-2"}>
+                    Approuver
+                </button>
+                <button className={"btn btn-danger d-block"}>
+                    Désapprouvé
+                </button>
+            </div>
         </div>
     );
 };
 
 Offer.propTypes = {
     position: PropTypes.string.isRequired,
-    salary: PropTypes.string.isRequired,
+    salary: PropTypes.number.isRequired,
     startDate: PropTypes.string.isRequired,
     endDate: PropTypes.string.isRequired,
 };
