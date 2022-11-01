38abb79e318d511453a66ee1979333cde3392f59
Jérémy Mailhot <jaytech64@pm.me>
2022-10-31 21:15:44 -0400

    Ajout d'un component pour pouvoir créer une signature.

---
+++ src/front-end/osekiller/src/components/Signature.js
@@ -0,0 +1,33 @@
+import React, { useRef, useState } from "react";
+import SignaturePad from "react-signature-canvas";
+
+const Signature = () => {
+    const sigPad = useRef({});
+    const [data, setData] = useState("");
+
+    return (
+        <div>
+            <SignaturePad
+                ref={sigPad}
+                backgroundColor={"white"}
+                style={{ backgroundColor: "#2C324C" }}
+            />
+            <div className={"d-flex"}>
+                <button
+                    className={"btn btn-primary me-1"}
+                    onClick={() => sigPad.current.clear()}
+                >
+                    Effacer
+                </button>
+                <button
+                    className={"btn btn-primary"}
+                    onClick={() => setData(sigPad.current.toDataURL())}
+                >
+                    Sauvegarder
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Signature;
