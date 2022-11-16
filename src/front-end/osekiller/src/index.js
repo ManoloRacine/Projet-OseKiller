d9fb0773f04bb6ebd44df2a0452fbf3676eca46c
Jérémy Mailhot <jaytech64@pm.me>
2022-11-15 23:13:49 -0500

    OS-116-120 PR ready

--- src/front-end/osekiller/src/index.js
+++ src/front-end/osekiller/src/index.js
@@ -211,7 +211,7 @@ root.render(
                     element={
                         <ProtectedRoute
                             authenticated
-                            allowedRoles={["MANAGER"]}
+                            allowedRoles={["MANAGER", "STUDENT", "COMPANY"]}
                         >
                             <AcceptedApplications />
                         </ProtectedRoute>
