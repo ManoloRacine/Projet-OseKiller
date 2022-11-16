570f3df64efa97285bdad90cddacb250f04ca3cf
ManoloRacine <manolo191202@gmail.com>
2022-11-15 22:12:19 -0500

    OS-133 dto inutilisé supprimés

--- src/main/java/com/osekiller/projet/controller/payload/request/GlobalAppreciationDto.java
+++
@@ -1,9 +0,0 @@
-package com.osekiller.projet.controller.payload.request;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
-public record GlobalAppreciationDto(@NotNull Integer fieldA,
-                                    @NotBlank String fieldB,
-                                    @NotNull Boolean fieldC) {
-}
