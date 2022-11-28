78af419106c5149b1ea5ba1ff21699bb08115f0e
ManoloRacine <89532459+ManoloRacine@users.noreply.github.com>
2022-11-28 11:36:50 -0500

    OS-144 ajout des notifications et du modal nécessaire (#85)
    * OS-144 ajout des notifications et du modal nécessaire
    
    * OS-144 ajustement des opérateurs d'égalité
    
    * OS-144 ajout du catch
    
    * OS-144 enlevé le catch

--- src/front-end/osekiller/src/services/UserService.js
+++ src/front-end/osekiller/src/services/UserService.js
@@ -1,17 +1,37 @@
-import axios from "../api/axios"
+import axios from "../api/axios";
 
 export const fetchUsers = () => {
-   return axios.get("/users", {
+    return axios.get("/users", {
         headers: {
             Authorization: localStorage.getItem("accessToken"),
         },
     });
-}
+};
 
 export const validateUser = (userId, valid) => {
-    return axios.post("/users/" + userId + "/validate",{validation: valid}, {
-         headers: {
-             Authorization: localStorage.getItem("accessToken"),
-         },
-     });
-}
+    return axios.post(
+        "/users/" + userId + "/validate",
+        { validation: valid },
+        {
+            headers: {
+                Authorization: localStorage.getItem("accessToken"),
+            },
+        }
+    );
+};
+
+export const getNotifications = () => {
+    return axios.get("/notifications", {
+        headers: {
+            Authorization: localStorage.getItem("accessToken"),
+        },
+    });
+};
+
+export const removeNotification = (id) => {
+    return axios.delete(`/notifications/${id}`, {
+        headers: {
+            Authorization: localStorage.getItem("accessToken"),
+        },
+    });
+};
--- src/front-end/osekiller/src/views/Dashboard.js
+++ src/front-end/osekiller/src/views/Dashboard.js
@@ -1,17 +1,76 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthenticatedUserContext } from "../App";
 import StudentDashboard from "../components/dashboard/student/StudentDashboard";
 import CompanyDashboard from "../components/dashboard/company/CompanyDashboard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { getNotifications, removeNotification } from "../services/UserService";
+import { Modal } from "react-bootstrap";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
+import { faX } from "@fortawesome/free-solid-svg-icons";
 
 const Dashboard = () => {
     const { authenticatedUser } = useContext(AuthenticatedUserContext);
 
+    const [notifications, setNotifications] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+
+    useEffect(() => {
+        getNotifications().then((response) => {
+            console.log(response.data);
+            setNotifications(response.data);
+        });
+    }, [authenticatedUser.id]);
+
+    const handleRemoveNotif = (id) => {
+        removeNotification(id).then(
+            setNotifications(
+                notifications.filter((notif) => notif.notificationId !== id)
+            )
+        );
+    };
+
     return (
         <div className="p-3">
             <h1>{`Bonjour, ${authenticatedUser.name}`}</h1>
-
+            <FontAwesomeIcon
+                icon={faBell}
+                className="fa-2x"
+                style={
+                    notifications.length === 0
+                        ? { color: "black" }
+                        : { color: "red" }
+                }
+                onClick={() => setShowModal(true)}
+            />
             {authenticatedUser.role === "STUDENT" && <StudentDashboard />}
             {authenticatedUser.role === "COMPANY" && <CompanyDashboard />}
+            <Modal
+                show={showModal}
+                onHide={() => setShowModal(false)}
+                size="xl"
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Notifications</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {notifications.map((notif, key) => (
+                        <div className="row p-4 border">
+                            <div className="col-11">
+                                {notif.notificationMessage}
+                            </div>
+                            <div className="col-1">
+                                <button
+                                    type="button"
+                                    className="btn-close"
+                                    onClick={() =>
+                                        handleRemoveNotif(notif.notificationId)
+                                    }
+                                ></button>
+                            </div>
+                        </div>
+                    ))}
+                </Modal.Body>
+            </Modal>
         </div>
     );
 };
